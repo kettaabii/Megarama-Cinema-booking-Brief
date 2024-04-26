ffb2446ff79039bcbef74b3e4dd7d9f4042e6029
saidtalfana <saidtalfana98@gmail.com>
2024-04-26 17:14:21 +0100

    second commit

--- src/main/java/Sevlets/AddReservation.java
+++ src/main/java/Sevlets/AddReservation.java
@@ -1,7 +1,11 @@
 package Sevlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.ReservationDAO;
+import MYSQLIMP.Connectiondb;
 import MYSQLIMP.ReservationDAOImpl;
-import MySQL.MySQLReservationDAO;
-import model.Reservation;
+import Modals.Reservation;
 
 /**
  * Servlet implementation class AddReservation
@@ -19,13 +24,20 @@ import model.Reservation;
 @WebServlet("/AddReservation")
 public class AddReservation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+	 private ReservationDAO  reservationDAO;
     public AddReservation() {
         super();
        
     }
-    public void init() {
-        reservationDAO = new ReservationDAOImpl();
+    public void init() throws ServletException {
+        super.init();
+        Connection connection = null;
+        try {
+            connection = Connectiondb.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle SQLException appropriately
+        }
+        reservationDAO = new ReservationDAOImpl(connection);
     }
 
 	
@@ -36,10 +48,10 @@ public class AddReservation extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String reservationId = request.getParameter("reservationId");
+		int reservationId = Integer.parseInt(request.getParameter("reservationId "));
         String viewer = request.getParameter("viewer");
         String filmTitle = request.getParameter("filmTitle");
-        Date reservationDate=request.getParameter("reservationDate");
+        String reservationDate=request.getParameter("reservationDate");
         int seat = Integer.parseInt(request.getParameter("seat"));
         int price = Integer.parseInt(request.getParameter("price"));
         
@@ -47,14 +59,25 @@ public class AddReservation extends HttpServlet {
         
         
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        
-        
-        
-        
-        Reservation reservation = new Reservation(reservationId, viewer, filmTitle, reservationDate, seat,price);
-        reservationDAO.createReservation(reservation);
+        try {
+            Date parsedStartDate = dateFormat.parse(reservationDate);
+           
 
-        response.sendRedirect(request.getContextPath() + "/t");
+            
+            Modals.Reservation reservation = new Reservation(reservationId, viewer, filmTitle, parsedStartDate, seat,price);
+            reservationDAO.insertReservation(reservation);
+
+            response.sendRedirect(request.getContextPath() + "/t");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        
+       
 	}
 
 }
